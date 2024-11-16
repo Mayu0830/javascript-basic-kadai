76839f0685f6f95e329c697215ac6eb111b98f52
Mayu0830 <mayu.0830.sunny@gmail.com>
2024-11-16 23:38:49 +0900

    kadai_005をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 体重を代入
+let bodyWeight = 68;
+
+// 身長を代入
+let height = 1.7;
+
+// BMIを代入
+let bmi = bodyWeight / (height * height);
+
+// それぞれの値をコンソールに出力
+console.log(bodyWeight);
+console.log(height);
+console.log(bmi);
